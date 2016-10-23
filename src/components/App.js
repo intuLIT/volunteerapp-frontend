1b9df4ff774da75f3f31ba06aceba25d2790d459
reese695 <reese695@gmail.com>
2016-10-22 17:39:12 -0700

    Working routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h2>Welcome!</h2>
-                <Link to="test">Test</Link>
                 {this.props.children}
             </div>
         )
