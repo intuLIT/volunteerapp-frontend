6c7d0d402d139da0a4ac713367452d7ee65316cd
Rushil Patel <rushil9@gmail.com>
2016-10-22 18:34:41 -0700

    home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import MainNavbar from './MainNavbar'
-import {Grid} from 'react-bootstrap';
+import {Grid, Jumbotron} from 'react-bootstrap';
 
 
 class Home extends React.Component {
     render() {
+      var centerStyle = {
+          textAlign: "center",
+      };
+
         return (
             <div>
-                <MainNavbar />
+                <MainNavbar/>
                 <Grid>
                     <p>Welcome to our website homepage!</p>
+                    <Jumbotron>
+                      <h1 style={centerStyle}>
+                          Vlntr
+                      </h1>
+                      <p> style={centerStyle}
+                          crowdsourcing volunteers
+                      </p>
+                    </Jumbotron>
                 </Grid>
             </div>
         )
